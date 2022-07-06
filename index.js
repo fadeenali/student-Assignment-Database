a508effb8d3c3c0410c856d382e6b79c007b84f3
fardeenali <fardeenali1602@gmail.com>
2022-07-07 00:45:27 +0530

    new changes

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 const Student_Controller = require("./student/controller/Student_Controller");
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
+
+
+
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Routes
 app.use("/student", Student_Controller);
